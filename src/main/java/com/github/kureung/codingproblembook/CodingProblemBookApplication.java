0c87fcbe17f12ec03d6ae1c231a27b2de21aa7a8
kureung <terry9611@naver.com>
2024-11-21 23:50:45 +0900

    build: init

---
+++ src/main/java/com/github/kureung/codingproblembook/CodingProblemBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.kureung.codingproblembook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingProblemBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingProblemBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kureung/codingproblembook/CodingProblemBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kureung.codingproblembook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingProblemBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
